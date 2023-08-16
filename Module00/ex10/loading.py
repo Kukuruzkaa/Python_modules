fd245dabfce2767244f93988e1c59e8249ad63b3
ddiakova <ddiakova@student.42.fr>
2023-08-16 23:06:04 +0200

    working loading bar

--- Module00/ex10/loading.py
+++ Module00/ex10/loading.py
@@ -1,43 +1,28 @@
 import time
-import sys
-
-# def ft_progress(lst):
-#     start_time = time.time()
-#     total = len(lst)
-#     length = 10
-#     for elem, index in enumerate(lst):
-#         progress = (index + 1) / total
-#         elapsed_time = time.time() - start_time
-#         eta = (elapsed_time / progress) - elapsed_time
-#         yield elem
-        
-#         bar = f"ETA: {eta:5.2f}s [{int(progress * 100):3d}%][{int(progress * length) * '=' + '>':{length}s}] {index:4d}/{total} | elapsed time {elapsed_time:.2f}s"
-#         sys.stdout.write(bar)
-#         sys.stdout.flush()
-#         sys.stdout.write("\r")
-    
 
 def ft_progress(list):
     start_time = time.time()
-    start = list[0]
-    end = list[len(list) - 1]
-    total = end - start
+    total = len(list)
     length = 20
     eta = 0
     
-    for index in range(start,end):
-        progress = (index / total) * 100
-        pr_bar = round((progress / 100) * length) 
+    if total == 0:
+        total = 1
+    for index in range(total):
+        elapsed_time = time.time() - start_time
+     
+        percentage = ((index + 1) / total) * 100
+        if percentage > 0:
+            eta = (elapsed_time * 100) / percentage
+        progress = int((percentage * length) / 100)
         yield index
         
-        c_bar = (f"ETA:{eta:5.2f}s [{int(progress):3d}%][{'=' * pr_bar}]")
+        c_bar = (f"ETA:{eta:.2f}s [{int(percentage):3d}%][{'=' * progress + '>':{length}s}] {index + 1:4d}/{total} | elapsed time {elapsed_time:.2f}s")
         print(c_bar, end="\r")
       
-        
-        
 if __name__ == "__main__":
     
-        listy = range(1000)
+        listy = range(100)
         ret = 0
         for elem in ft_progress(listy):
             ret += (elem + 3) % 5
