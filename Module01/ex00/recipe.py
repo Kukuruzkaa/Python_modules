948f030820325f052205148b1ebcbf10f8739b2d
ddiakova <ddiakova@student.42.fr>
2023-08-23 20:43:22 +0200

    recipe class init method to implement

---
+++ Module01/ex00/recipe.py
@@ -0,0 +1,16 @@
+class Recipe:
+    name = ""
+    cooking_lvl = range(1,5)
+    cooking_time = 0
+    ingridients = []
+    description = ""
+    recipe_type = ""
+
+    def __str__(self):
+        """Return the string to print with the recipe info"""
+        txt = ""
+        """Your code here"""
+        return txt
+
+
+
