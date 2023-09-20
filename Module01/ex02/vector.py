b0fa8289a46c73522f162acde129f05f7d41d907
ddiakova <ddiakova@student.42.fr>
2023-09-20 21:32:50 +0200

    T method + tests to do

--- Module01/ex02/vector.py
+++ Module01/ex02/vector.py
@@ -31,7 +31,7 @@ class Vector:
     def is_tuple(values):
         if len(values) == 2:
             a, b = values
-            if not isinstance(a, int) or not isinstance(b) or a < b:
+            if not isinstance(a, int) or not isinstance(b, int) or a < b:
                 raise ValueError("Invalid range")
             return True
         return False
@@ -43,88 +43,102 @@ class Vector:
         return False
 
 
-# import math
+    def dot(self, other):
+        if not isinstance(other, Vector) and self.shape == other.shape:
+            raise ValueError("Wrong vector types.")
+        else:
+            res = 0
+            tuple_pairs = zip(self, other)
+            for el1, el2 in tuple_pairs:
+                pairs = zip(el1, el2)
+                for x, y in pairs:
+                    res += x * y
+                    return res
+                
+    def __add__(self, other):
+        """Vector addition."""
+        if not isinstance(other, Vector) and self.shape == other.shape:
+            raise ValueError("Wrong vector types.")
+        else:
+            res = []
+            tuple_pairs = zip(self, other)
+            for el1, el2 in tuple_pairs:
+                pairs = zip(el1, el2)
+                new_list = [x + y for x, y in pairs]
+                res.append(new_list)
+            return Vector(res)
+        
+    def __radd__(self, other):
+        """Vector addition."""
+        return self.__add__(other)
+        
+    def __sub__(self, other):
+        """Vector subtraction."""
+        if not isinstance(other, Vector) and self.shape == other.shape:
+            raise ValueError("Wrong vector types.")
+        else:
+            res = []
+            tuple_pairs = zip(self, other)
+            for el1, el2 in tuple_pairs:
+                pairs = zip(el1, el2)
+                new_list = [x - y for x, y in pairs]
+                res.append(new_list)
+            return Vector(res)
+    
+    def __rsub__(self, other):
+        """Vector subtraction."""
+        return self.__sub__(other)
+    
+    def __str__(self):
+        return str(self.values)  
+                    
+    def __mul__(self, scalar):
+        """Multiplication of a vector by a scalar."""
+        if not isinstance(scalar, int) or isinstance(scalar, float):
+            raise ValueError('Can only multiply Vector by a scalar')  
+        else:
+            res = []
+            for col in self.values:
+                new_col = []
+                for el in col:
+                    new_el = el * scalar
+                    new_col.append(new_el)
+                res.append(new_col)
+            return Vector(res)
+    
+    def __rmul__(self, scalar):
+        """Multiplication of a vector by a scalar."""
+        return self.__mul__(scalar)
+    
+    def __truediv__(self, scalar):
+        """True division of the vector by a scalar.""" 
+        if not isinstance(scalar, (int, float)):
+            raise ValueError('Can only devide Vector by a scalar')
+        else:
+            res = []
+            for col in self.values:
+                new_col = []
+                for el in col:
+                    new_el = el / scalar
+                    new_col.append(new_el)
+                res.append(new_col)
+            return Vector(res)
+    
+    def __rtruediv__(self, scalar):
+        raise NotImplementedError("Division of a scalar by a Vector is not defined here.")
+            
+    def __repr__(self):
+        """Unambiguous string representation of the vector."""
+        return self.__str__(self)
 
-# class Vector2D:
-#     """A two-dimensional vector with Cartesian coordinates."""
+    def T(self):
+        if self.shape == 1:
+            new_col = []
+            for el in self.values[0]:
+                new_col.append([el])
+            return Vector(new_col)
+                
+                
+                
+    
 
-#     def __init__(self, x, y):
-#         self.x, self.y = x, y
-
-#     def __str__(self):
-#         """Human-readable string representation of the vector."""
-#         return '{:g}i + {:g}j'.format(self.x, self.y)
-
-#     def __repr__(self):
-#         """Unambiguous string representation of the vector."""
-#         return repr((self.x, self.y))
-
-#     def dot(self, other):
-#         """The scalar (dot) product of self and other. Both must be vectors."""
-
-#         if not isinstance(other, Vector2D):
-#             raise TypeError('Can only take dot product of two Vector2D objects')
-#         return self.x * other.x + self.y * other.y
-#     # Alias the __matmul__ method to dot so we can use a @ b as well as a.dot(b).
-#     __matmul__ = dot
-
-#     def __sub__(self, other):
-#         """Vector subtraction."""
-#         return Vector2D(self.x - other.x, self.y - other.y)
-
-#     def __add__(self, other):
-#         """Vector addition."""
-#         return Vector2D(self.x + other.x, self.y + other.y)
-
-#     def __mul__(self, scalar):
-#         """Multiplication of a vector by a scalar."""
-
-#         if isinstance(scalar, int) or isinstance(scalar, float):
-#             return Vector2D(self.x*scalar, self.y*scalar)
-#         raise NotImplementedError('Can only multiply Vector2D by a scalar')
-
-#     def __rmul__(self, scalar):
-#         """Reflected multiplication so vector * scalar also works."""
-#         return self.__mul__(scalar)
-
-#     def __neg__(self):
-#         """Negation of the vector (invert through origin.)"""
-#         return Vector2D(-self.x, -self.y)
-
-#     def __truediv__(self, scalar):
-#         """True division of the vector by a scalar."""
-#         return Vector2D(self.x / scalar, self.y / scalar)
-
-#     def __mod__(self, scalar):
-#         """One way to implement modulus operation: for each component."""
-#         return Vector2D(self.x % scalar, self.y % scalar)
-
-#     def __abs__(self):
-#         """Absolute value (magnitude) of the vector."""
-#         return math.sqrt(self.x**2 + self.y**2)
-
-#     def distance_to(self, other):
-#         """The distance between vectors self and other."""
-#         return abs(self - other)
-
-#     def to_polar(self):
-#         """Return the vector's components in polar coordinates."""
-#         return self.__abs__(), math.atan2(self.y, self.x)
-
-
-# if __name__ == '__main__':
-#     v1 = Vector2D(2, 5/3)
-#     v2 = Vector2D(3, -1.5)
-#     print('v1 = ', v1)
-#     print('repr(v2) = ', repr(v2))
-#     print('v1 + v2 = ', v1 + v2)
-#     print('v1 - v2 = ', v1 - v2)
-#     print('abs(v2 - v1) = ', abs(v2 - v1))
-#     print('-v2 = ', -v2)
-#     print('v1 * 3 = ', v1 * 3)
-#     print('7 * v2 = ', 7 * v1)
-#     print('v2 / 2.5 = ', v2 / 2.5)
-#     print('v1 % 1 = ', v1 % 1)
-#     print('v1.dot(v2) = v1 @ v2 = ', v1 @ v2)
-#     print('v1.distance_to(v2) = ',v1.distance_to(v2))
-#     print('v1 as polar vector, (r, theta) =', v1.to_polar())
